a98f9103d12af9d2efdbdfd56a1c9c99f66b878e
Misha Lel <lelmisa566@gmail.com>
2025-09-29 16:59:08 +0300

    Задание томаты

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
-import { power } from './ functions.js';
-const result = power(2, 10);
-console.log(result);
+const tomatoPrice = (kilo, price) => {
+  return kilo * 450;
+};
+console.log(tomatoPrice(1)); // 450
+console.log(tomatoPrice(0.65)); // 292.5
+console.log(tomatoPrice(3.289)); // 1480.05
+
+console.log(tomatoPrice(0, 50)); // 0
+console.log(tomatoPrice(1, 22.5)); // 22.5
+console.log(tomatoPrice(14.96, 21.0007)); // 314.170472
