a3f7f8979ffced77b7d919088bae5eadd088652f
Misha Lel <lelmisa566@gmail.com>
2025-10-16 18:23:58 +0300

    задание Чётное число

---
+++ src/zad.isEven.js
@@ -0,0 +1,9 @@
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+console.log(isEven(4));
+console.log(isEven(7));
+console.log(isEven(0));
+console.log(isEven(-2));
+console.log(isEven(15));
