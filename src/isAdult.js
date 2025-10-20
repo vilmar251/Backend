c528581d1b8ca58080b1b6f1e434770317c19be7
Misha Lel <lelmisa566@gmail.com>
2025-10-20 17:55:53 +0300

    Задание Совершеннолетний или нет

---
+++ src/isAdult.js
@@ -0,0 +1,16 @@
+import chalk from 'chalk';
+
+function isAdult(age) {
+  if (age >= 18) {
+    console.log(chalk.green('Пользователь совершеннолетний'));
+    return true;
+  } else {
+    console.log(chalk.red('Пользователь несовершеннолетний'));
+    return false;
+  }
+}
+
+const randomAge = 15 + Math.floor(Math.random() * 8);
+
+console.log(`Возраст: ${randomAge}`);
+isAdult(randomAge);
